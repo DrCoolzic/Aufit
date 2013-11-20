a683cae7f2afbe26e42206590d7602d231089e60
DrCoolZic <DrCoolZic@hotmail.com>
2013-11-20 16:59:31 +0100

    Fix Visual Studio solution/project problems

--- KFStreamInfo/KFStreamInfo.cs
+++ KFStreamInfo/KFStreamInfo.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿/*!
+@file KFStreamInfo.cs
+@brief Test program of the KFStreamReader code
+
+<div class="jlg">Copyright (C) 2011 Software Preservation Society & Jean 
+Louis-Guerin\n\n This file is part of the Atari Universal FD Image Tools 
+project.\n\n The Atari Universal FD Image Tools project may be used and 
+distributed without restriction provided that this copyright statement is 
+not removed from the file and that any derivative work contains the 
+original copyright notice and the associated disclaimer.\n\n 
+
+The Atari Universal FD Image Tools project are free software; you can 
+redistribute it and/or modify  it under the terms of the GNU General Public 
+License as published by the Free Software Foundation; either version 2 of 
+the License, or (at your option) any later version.\n\n 
+The Atari Universal FD Image Tools project is distributed in the hope that 
+it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n See the 
+GNU General Public License for more details.\n\n 
+
+You should have received a copy of the GNU General Public License along 
+with the Atari Universal FD Image Tools project; if not, write to the Free 
+Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  
+02110-1301  USA\n</div>
+
+This file contains some code to test the KFStreamReader library.
+For more information see @ref progdes
+@author Jean Louis-Guerin
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
